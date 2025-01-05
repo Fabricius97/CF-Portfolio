ae5ea6b36728409e9abcdec9b3dc221a75cd0edd
Fabricius97 <christoffer.fabricius@hotmail.com>
2025-01-05 18:32:58 +0100

    navbar taking place

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,31 @@ import { Link } from "react-router-dom";
 import { styles } from "../style";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import { nav } from "framer-motion/client";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState("");
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="CF-Logo" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Christoffer <span className="sm:block hidden">| Fabricius</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
